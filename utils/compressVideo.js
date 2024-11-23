f0567661a2fe95701230a700b092503d47d21fc8
Shubham Dubey <159176664+Shubham32142@users.noreply.github.com>
2024-11-23 11:23:59 +0530

    Update compressVideo.js

--- utils/compressVideo.js
+++ utils/compressVideo.js
@@ -19,12 +19,13 @@ export async function compressVideo(inputPath, chunkDir, chunkSize = 10) {
         "-f",
         "segment", // Use segmenting format
         "-reset_timestamps",
-        "1" // Reset timestamps for each chunk
+        "1" ,
+        "-preset", "fast"// Reset timestamps for each chunk
       ).videoCodec('libx264')
       .audioCodec('aac')
       .audioBitrate('128k')
       .size("50%")
-      .preset('fast')
+     
        // Compress size to 70%
       .on("end", () => {
         console.log("Splitting and compression complete");
